b6da6f014ef82aa279dfb9e6227f9ace1fea190d
Kama580 <kleetal@gmail.com>
2021-03-29 14:47:49 -0400

    feat: miles per year user input

--- src/App.js
+++ src/App.js
@@ -13,40 +13,55 @@ class App extends React.Component {
 		super();
 		this.state = {
 			rates: ["flat", "TOU"],
-			timesOfDay: { "12to18": "high", "18to12": "low" },
-			currentRate: "",
-			milesPerYear: 0,
+			timesOfDay: {
+				"Between noon and 6pm": "high",
+				"After 6pm and before noon": "low",
+			},
+			currentRate: "flat",
+			milesPerYear: 10000,
 			chargeTimeWindow: "",
 			bestPlan: "",
 		};
-		this.setCurrentRate = this.setCurrentRate.bind(this);
+		// this.setCurrentRate = this.setCurrentRate.bind(this);
+		// this.setMilesPerYear = this.setMilesPerYear.bind(this);
+		this.handleUserInput = this.handleUserInput.bind(this);
 	}
+	componentDidUpdate() {
+		console.log("state:", this.state);
+	}
+	// setCurrentRate(rate) {
+	// 	this.setState({
+	// 		currentRate: rate,
+	// 	});
+	// }
 
-	setCurrentRate(rate) {
-		this.setState({
-			currentRate: rate,
-		});
+	handleUserInput(event) {
+		this.setState({ [event.target.name]: event.target.value });
 	}
 
 	render() {
 		return (
 			<div className="App">
-				<div className="form">
+				<form>
 					form
-					<form>
-						<label htmlFor="currentRate">Your current plan</label>
-						<select
-							name="currentRate"
-							onChange={(event) => {
-								this.setCurrentRate(event.target.value);
-							}}
-						>
-							{this.state.rates.map((rate, idx) => (
-								<option key={idx}>{rate}</option>
-							))}
-						</select>
-					</form>
-				</div>
+					<label htmlFor="currentRate">
+						What is your current electricity rate?
+					</label>
+					<select name="currentRate" onChange={this.handleUserInput}>
+						{this.state.rates.map((rate, idx) => (
+							<option key={idx}>{rate}</option>
+						))}
+					</select>
+					<label htmlFor="milesPerYear">
+						How many miles will you be driving per year?
+					</label>
+					<input
+						type="text"
+						name="milesPerYear"
+						onChange={this.handleUserInput}
+						value={this.state.milesPerYear}
+					/>
+				</form>
 				<div className="chart">chart</div>
 				<div className="suggestion">suggestion</div>
 			</div>
