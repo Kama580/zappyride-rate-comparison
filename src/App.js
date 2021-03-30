4458cd5fc3721eeabfaedefd8bccde0a1a218cb9
Kama580 <kleetal@gmail.com>
2021-03-30 01:00:58 -0400

    chore: some basic css

--- src/App.js
+++ src/App.js
@@ -121,62 +121,70 @@ class App extends React.Component {
 
 		return (
 			<div className="App">
-				<form>
-					form
-					<p>
-						<label htmlFor="currentRate">
-							What is your current electricity rate?
-						</label>
-						<select name="currentRate" onChange={this.handleUserInput}>
-							{ratesArray.map((rate, idx) => (
-								<option key={idx}>{rate}</option>
-							))}
-						</select>
-					</p>
-					<p>
-						<label htmlFor="milesPerYear">
-							How many miles will you be driving per year?
-						</label>
-						<input
-							type="text"
-							name="milesPerYear"
-							onChange={this.handleUserInput}
-							value={this.state.milesPerYear}
-						/>
-					</p>
-					<p>
-						<label htmlFor="chosenTimeWindow">
-							What time of the day do you plan to charge the EV?
-						</label>
-					</p>
-					<select name="chosenTimeWindow" onChange={this.handleUserInput}>
-						{timesOfDayOptionsArray.map((time, idx) => (
-							<option key={idx}>{time}</option>
-						))}
-					</select>
-					<button type="submit">Calculate</button>
-				</form>
-				{this.state.loaded && (
-					<div>
-						<Chart
-							ratesArray={ratesArray}
-							B1={this.state.B1}
-							billImpact={this.state.billImpact}
-						/>
-						<div className="suggestion">
-							<Suggestion
-								currentAnnualCost={this.state.currentAnnualCost}
-								newAnnualCost={this.state.newAnnualCost}
-								bestPlan={this.state.bestPlan}
-								currentPlan={
-									this.state.B2.filter((rate) => {
-										return rate.name === this.state.currentRate;
-									})[0]
-								}
+				<div>
+					<form className="left-section">
+						<p>
+							<label htmlFor="currentRate">
+								Which rate are you currently on?
+							</label>
+							<p>
+								<select name="currentRate" onChange={this.handleUserInput}>
+									{ratesArray.map((rate, idx) => (
+										<option key={idx}>{rate}</option>
+									))}
+								</select>
+							</p>
+						</p>
+						<p>
+							<label htmlFor="milesPerYear">
+								How many miles will you be driving per year?
+							</label>
+							<p>
+								<input
+									type="text"
+									name="milesPerYear"
+									onChange={this.handleUserInput}
+									value={this.state.milesPerYear}
+								/>
+							</p>
+						</p>
+						<p>
+							<label htmlFor="chosenTimeWindow">
+								What hours of the day do you plan to charging your EV?
+							</label>
+						</p>
+						<p>
+							<select name="chosenTimeWindow" onChange={this.handleUserInput}>
+								{timesOfDayOptionsArray.map((time, idx) => (
+									<option key={idx}>{time}</option>
+								))}
+							</select>
+						</p>
+					</form>
+				</div>
+				<div className="right-section">
+					{this.state.loaded && (
+						<div>
+							<Chart
+								ratesArray={ratesArray}
+								B1={this.state.B1}
+								billImpact={this.state.billImpact}
 							/>
+							<div className="suggestion">
+								<Suggestion
+									currentAnnualCost={this.state.currentAnnualCost}
+									newAnnualCost={this.state.newAnnualCost}
+									bestPlan={this.state.bestPlan}
+									currentPlan={
+										this.state.B2.filter((rate) => {
+											return rate.name === this.state.currentRate;
+										})[0]
+									}
+								/>
+							</div>
 						</div>
-					</div>
-				)}
+					)}
+				</div>
 			</div>
 		);
 	}
--- src/chart.js
+++ src/chart.js
@@ -11,16 +11,46 @@ const Chart = (props) => {
 	return (
 		<div className="chart">
 			<VictoryChart
-				domainPadding={40}
+				height={400}
 				width={500}
-				height={600}
+				domainPadding={{ x: 100, y: [0, 20] }}
 				theme={VictoryTheme.material}
 			>
-				<VictoryAxis tickValues={props.ratesArray} />
-				<VictoryAxis dependentAxis tickFormat={(x) => `$${x}/year`} />
+				<VictoryAxis tickValues={props.ratesArray} stile={{ stroke: "none" }} />
+				<VictoryAxis
+					dependentAxis
+					tickFormat={(x) => `$${x}/year`}
+					padding={{ left: 20, right: 60 }}
+					name="name"
+					style={{
+						tickLabels: {
+							fontSize: 8,
+							align: "center",
+							padding: 0,
+							stroke: "none",
+						},
+					}}
+				/>
 				<VictoryStack>
-					<VictoryBar data={props.B1} x="name" y="cost" />
-					<VictoryBar data={props.billImpact} x="name" y="cost" />
+					<VictoryBar
+						data={props.B1}
+						x="name"
+						y="cost"
+						left={50}
+						animate={{
+							duration: 2000,
+							onLoad: { duration: 1000 },
+						}}
+					/>
+					<VictoryBar
+						data={props.billImpact}
+						x="name"
+						y="cost"
+						animate={{
+							duration: 2000,
+							onLoad: { duration: 1000 },
+						}}
+					/>
 				</VictoryStack>
 			</VictoryChart>
 		</div>
